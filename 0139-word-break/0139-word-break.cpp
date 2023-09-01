4b34ed3f27dc480136555b468f5df3b2d855d396
Sooraj <86311576+sooraj-24@users.noreply.github.com>
2023-09-01 14:03:45 +0530

    Time: 9 ms (59.61%), Space: 7.6 MB (89.07%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -2,8 +2,22 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        vector<int> dp(n + 1, -1);
-        return rec(n-1, s, wordDict, dp);
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for(int ind=1; ind<=n; ind++){
+            for(int i=0; i<wordDict.size(); i++){
+                int n = wordDict[i].length();
+                if(ind - n >= 0){
+                    if(s.substr(ind-n, n) == wordDict[i]){
+                        if(dp[ind - n]) dp[ind] = 1;
+                    }
+                }
+            }
+        }
+        for(int i=0; i<=n; i++){
+            cout<<dp[i]<<" ";
+        }
+        return dp[n];
     }
     bool rec(int ind, string s, vector<string>& wordDict, vector<int>& dp){
         if(ind < 0) return dp[ind + 1] = true;
