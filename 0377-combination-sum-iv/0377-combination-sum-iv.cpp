a82da5887480d19ce2783bf4df4a0f15da9f410a
Sooraj <86311576+sooraj-24@users.noreply.github.com>
2023-09-06 19:52:26 +0530

    Time: 3 ms (49.96%), Space: 6.7 MB (45.42%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target + 1, -1);
-        // dp[0] = 1;
-        // for(int i=1; i<=target; i++){
-        //     int val = 0;
-        //     for(int j=0; j<nums.size(); j++){
-        //         if(i >= nums[j]){
-        //             val += dp[i - nums[j]];
-        //         }
-        //     }
-        //     dp[i] = val;
-        // }
-        return rec(target, nums, dp);
-    }
-    int rec(int target, vector<int> nums, vector<int>& dp){
-        if(target == 0) return dp[0] = 1;
-        if(dp[target] != -1) return dp[target];
-        int val = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(target >= nums[i]){
-                val += rec(target - nums[i], nums, dp);
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for(int i=1; i<=target; i++){
+            long long val = 0;
+            for(int j=0; j<nums.size(); j++){
+                if(i >= nums[j]){
+                    val += dp[i - nums[j]];
+                }
             }
+            dp[i] = val;
         }
-        return dp[target] = val;
+        return dp[target];
     }
+    // int rec(int target, vector<int> nums, vector<int>& dp){
+    //     if(target == 0) return dp[0] = 1;
+    //     if(dp[target] != -1) return dp[target];
+    //     int val = 0;
+    //     for(int i=0; i<nums.size(); i++){
+    //         if(target >= nums[i]){
+    //             val += rec(target - nums[i], nums, dp);
+    //         }
+    //     }
+    //     return dp[target] = val;
+    // }
 };
